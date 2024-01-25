2f7ac9517a84aa6dc59989c17b9e26a06cf89363
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2024-01-25 10:05:39 +0530

    hot fix: use verifyJWT for logout request

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { forgetPassword, loginUser, logoutUser, registerUser, resetPassword } from "../controllers/users.controller.js";
 import { loginValidationRules, registerValidationRules } from "../middlewares/userValidation.middlewares.js";
 import { validate } from "../middlewares/validation.middlewares.js";
+import verfiyJWT from "../middlewares/auth.middlewares.js"
 
 const router = Router()
 
 router.route('/register').post(registerValidationRules(), validate, registerUser)
 router.route('/login').post(loginValidationRules(), validate, loginUser)
-router.route('/logout').delete(logoutUser)
+router.route('/logout').delete(verfiyJWT, logoutUser)
 router.route('/forget-password').post(forgetPassword)
 router.route('/reset-password').post(resetPassword)
 
