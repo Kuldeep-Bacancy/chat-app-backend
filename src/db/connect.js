67719729c2288dbc7ccc51c7acf10dd5f5b1d312
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2024-01-25 11:10:24 +0530

    access database name from env

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js"
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(`${process.env.DB_URL}/${DB_NAME}`)
+    const connect = await mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`)
     console.log(`DB Connection Successfull: ${connect.connection.host}`);
   } catch (error) {
     console.log(`DB Connection Failed: ${error.message}`);
