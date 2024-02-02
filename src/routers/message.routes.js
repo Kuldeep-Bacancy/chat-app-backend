b59cfad7815823526bacd29ed0a64558bd386423
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2024-02-02 10:36:16 +0530

    Chanegs send Message function to handle attachments

--- src/routers/message.routes.js
+++ src/routers/message.routes.js
@@ -1,11 +1,12 @@
 import { getAllMessages, sendMessage } from "../controllers/messages.controller.js";
 import verfiyJWT from "../middlewares/auth.middlewares.js";
+import { upload } from "../middlewares/multer.middlewares.js";
 
 import { Router } from "express";
 
 const router = Router()
 
-router.route('/').post(verfiyJWT, sendMessage)
+router.route('/').post(verfiyJWT, upload.array('attachments', 2), sendMessage)
 router.route('/:chatId').get(verfiyJWT, getAllMessages)
 
 export default router
