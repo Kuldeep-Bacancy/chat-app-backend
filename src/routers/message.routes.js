c70f3419cb6ddb426770ab3e6060358376761ab9
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2024-02-05 10:28:52 +0530

    multiple images as attachments while sending message

--- src/routers/message.routes.js
+++ src/routers/message.routes.js
@@ -6,7 +6,7 @@ import { Router } from "express";
 
 const router = Router()
 
-router.route('/').post(verfiyJWT, upload.array('attachments', 2), sendMessage)
+router.route('/').post(verfiyJWT, upload.array('attachments[]', 2), sendMessage)
 router.route('/:chatId').get(verfiyJWT, getAllMessages)
 
 export default router
