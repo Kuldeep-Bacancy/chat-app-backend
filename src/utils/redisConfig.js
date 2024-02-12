b0e5e42a7a6319acc543e9c4e12974491cffeafb
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2024-02-12 10:01:18 +0530

    Changes in redis config

--- src/utils/redisConfig.js
+++ src/utils/redisConfig.js
@@ -1,10 +1,3 @@
 import { Redis } from "ioredis";
 
-
-const redisOptions = process.env.ENV == 'development' ? process.env.REDIS_URL : {
-  password: process.env.REDIS_PASSWORD,
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
-}
-
 export const redisConnection = new Redis(process.env.REDIS_URL);
