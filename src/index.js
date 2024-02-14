d4a54141043a4fa04bae32af01110b8eec5571b5
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2024-02-14 16:19:49 +0530

    Added events in socket for chat notifications

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import app from './app.js'
 import { Server } from 'socket.io';
 import { createServer } from "http";
 import ApiResponse from './utils/ApiResponse.js';
+import ApiError from './utils/ApiError.js';
 
 const server = createServer(app)
 const io = new Server(server)
@@ -31,21 +32,61 @@ connectDB()
 })
 
 io.on('connection', (socket) => {
-  socket.on('join-chat', (room) => {
-    socket.join(room)
-    console.log(`----------------------User (${socket.id}) joined room: ${room}----------------`)
-  })
+  try {
+    console.log(`⚡: ${socket.id} user just connected`);
 
-  socket.on('typing',(room) => {
-    socket.in(room).emit('typing')
-  })
+    socket.on("setup", async (userInfo) => {
+      socket.user = userInfo
 
-  socket.on('stop typing', (room) => {
-    socket.in(room).emit('stop typing')
-  })
+      // We are creating a room with user id so that if user is joined but does not have any active chat going on.
+      // still we want to emit some socket events to the user.
+      // so that the client can catch the event and show the notifications.
+      socket.join(userInfo._id.toString());
+    })
 
-  socket.on('new-message', (newMessage) => {
-    let chat = newMessage?.chat
-    socket.in(chat?._id).emit('message-received', newMessage)
-  })
+    socket.on('join-chat', (room) => {
+      socket.join(room)
+      console.log(`----------------------User (${socket.id}) joined room: ${room}----------------`)
+    })
+
+    socket.on('typing', (room) => {
+      socket.in(room).emit('typing')
+    })
+
+    socket.on('stop typing', (room) => {
+      socket.in(room).emit('stop typing')
+    })
+
+    socket.on('new-message', (newMessage) => {
+      let chat = newMessage?.chat
+      socket.in(chat?._id).emit('message-received', newMessage)
+      socket.emit('notification-received', newMessage)
+    })
+
+    socket.on('delete-message', (deleteMessage) => {
+      let chat = deleteMessage?.chat
+      socket.in(chat).emit('message-deleted')
+    })
+
+    socket.on('notification-sent', (msgData) => {
+      const users =  msgData.chat.users
+      users.forEach(user => {
+        if(user != msgData.sender._id){
+          socket.in(user).emit('notification-received', msgData)
+        }
+      });
+    })
+
+    socket.on('disconnect', () => {
+      if (socket.user?._id) {
+        socket.leave(socket.user._id);
+      }
+      console.log(`A ${socket.id} user disconnected`);
+    })
+  } catch (error) {
+    new ApiError(
+      500,
+      "Something went wrong!"
+    );
+  }
 });
